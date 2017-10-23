92043698f6e9b754311dd9d564a6ad15c3c68529
Sebastien Ong Tone <s.ongtone@hotmail.com>
2017-10-22 21:15:02 -0400

    added some stuff to specify where some functions are going to be

--- gameStart/gameStart/gameStart.cpp
+++ gameStart/gameStart/gameStart.cpp
@@ -19,7 +19,7 @@ int main()
 void gameStart() {
 	cout << "WELCOME TO RISK!\n\n\n" << endl;
 	cout << "Please select a map!" << endl;
-	cout << "Map loader function here" << endl;
+	cout << "Map loader function here" << endl;//have a way to have the map loader choose the map here.
 
 	while (playerNumb) {
 		cout << "Please select a number of players! (2-6)" << endl;
@@ -30,19 +30,29 @@ void gameStart() {
 		}
 		else {
 			playerNumb = false;
-			cout << "You have selected " << playerNumber << " players!" << endl;
-			switch (playerNumber) {
+			cout << "You have selected " << playerNumber << " players!" << endl;// create the players.
+
+			switch (playerNumber) {//create the stuff for the players.
 			case 2: cout << "Each player will receive 40 armies" << endl;
+				cout << "Assign the players their own dice rolling faciities, assign each an empty card hand and create deck of cards." << endl;
+				cout << "For " << playerNumber << " players." << endl;
 				break;
 			case 3: cout << "Each player will receive 35 armies" << endl;
+				cout << "Assign the players their own dice rolling faciities, assign each an empty card hand and create deck of cards." << endl;
+				cout << "For " << playerNumber << " players." << endl;
 				break;
 			case 4: cout << "Each player will receive 30 armies" << endl;
+				cout << "Assign the players their own dice rolling faciities, assign each an empty card hand and create deck of cards." << endl;
+				cout << "For " << playerNumber << " players." << endl;
 				break;
 			case 5: cout << "Each player will receive 25 armies" << endl;
+				cout << "Assign the players their own dice rolling faciities, assign each an empty card hand and create deck of cards." << endl;
+				cout << "For " << playerNumber << " players." << endl;
 				break;
 			case 6: cout << "Each player will receive 20 armies" << endl;
+				cout << "Assign the players their own dice rolling faciities, assign each an empty card hand and create deck of cards." << endl;
+				cout << "For " << playerNumber << " players." << endl;
 				break;
-			
 			}
 		}
 
