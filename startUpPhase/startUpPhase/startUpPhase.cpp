884cfc406be88003511111496024975f8bcf1ec9
Sebastien Ong Tone <s.ongtone@hotmail.com>
2017-10-23 02:30:35 -0400

    Working on creating an algorithm that will determine the turn order

--- startUpPhase/startUpPhase/startUpPhase.cpp
+++ startUpPhase/startUpPhase/startUpPhase.cpp
@@ -1,6 +1,7 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <string>
 #include <time.h>
 using namespace std;
 
@@ -9,13 +10,17 @@ bool turnOrder = true;
 void startUp(int numberOfPlayers);
 
 class players {
+	int rolled;
 public:
-	int playerRoll;
+	void playerRoll(int roll) {
+		rolled = roll;
+	}
+	int getPlayerRoll() { return rolled; }
+	string p;
 };
 
 int main()
 {
-	
 	cout <<"How many players?" << endl;
 	cin >> numPlay;
 	startUp(numPlay);
@@ -25,16 +30,40 @@ int main()
 
 void startUp(int numberOfPlayers) {
 	srand(time(NULL));
+	int size;
+	int n;
 	switch (numberOfPlayers) {
 	case 2: 
-		players player1;
-		players player2;
+		players player[2];
+		n = sizeof(player)/sizeof(player[0]);
+		cout << "Size is " << n << endl;
+
+		player[0].p = "Player 1";
+		player[1].p = "Player 2";
 		while (turnOrder) {//determining the turn order. we can put the players in an array.
-			player1.playerRoll = rand() % 6 + 1;
-			cout << "Player 1 Rolled: " << player1.playerRoll << endl;
-			player2.playerRoll = rand() % 6 + 1;
-			cout << "Player 2 Rolled: " << player2.playerRoll << endl;
-			if (player1.playerRoll > player2.playerRoll) {
+			for (int i = 0; i < 2; i++) {
+				player[i].playerRoll(rand() % 6 + 1);
+				cout << player[i].p << " Rolled: " << player[i].getPlayerRoll() << endl;
+				turnOrder = false;
+			}
+			
+				/*for (int i = 0; i < n - 1; i++) {
+					for (int j = 0; j < n - 1; j++) {
+						if (arr[j] > arr[j + 1]) {
+
+						}
+					}
+				}
+			
+			void swapping(int *first, int *second) {
+				int temp = *first;
+				*second = *first;
+				*first = temp;
+
+			}*/
+
+		
+			/*if (player1.playerRoll > player2.playerRoll) {
 				cout << "Player 1 starts! Followed by Player 2" << endl;
 				turnOrder = false;
 			}
@@ -44,8 +73,7 @@ void startUp(int numberOfPlayers) {
 			}
 			else {
 				cout << "It's a tie! Reroll!" << endl;
-			}
-
+			}*/
 		}
 		break;
 	/*case 3:
@@ -77,5 +105,6 @@ void startUp(int numberOfPlayers) {
 	}
 
 }
+
 
 
