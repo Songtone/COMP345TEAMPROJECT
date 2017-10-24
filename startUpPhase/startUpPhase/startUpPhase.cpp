ca3fcc29c3003b96d560cd5d2bfb5faa59b6e0e8
Sebastien Ong Tone <s.ongtone@hotmail.com>
2017-10-23 21:56:41 -0400

    added attributes and setters and getters for the armies to be placed

--- startUpPhase/startUpPhase/startUpPhase.cpp
+++ startUpPhase/startUpPhase/startUpPhase.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int numPlay;
 int numberOfCountries;
 int numberOfCountriesAssigned;
+int numberOfCountriesOwned;
 bool playerNumber = true;
 bool turnOrder = true;
 void startUp(int numberOfPlayers);
@@ -16,6 +17,7 @@ void startUp(int numberOfPlayers);
 class players {
 	int rolled;
 	int army;
+	int countriesOwned;
 	string name;
 public:
 	void playerRoll(int roll) {//the players roll for the turn order
@@ -29,18 +31,24 @@ public:
 	void setArmiesToPlace(int a) {
 		army = a;
 	}
-	int getArmies() { return army; }
-	players() : rolled(0),army(0),name("Player 0") {}//default constructor
+	int getArmiesToPlace() { return army; }
+	void setNumberOfCountriesOwned(int c) {
+		countriesOwned = c;
+	}
+	int getNumberOfCountriesOwned() { return countriesOwned; }
+	players() : rolled(0),army(0),name("Player 0"),countriesOwned(0) {}//default constructor
+	
 	players(const players& p)//copy constructor
 	{
 		name = p.name;
 		rolled = p.rolled;
 		army = p.army;
+		countriesOwned = p.countriesOwned;
 	}
 };
 int main()
 {
-	while (playerNumber) {
+	while (playerNumber) {//loop for the start up function
 		cout << "How many players?" << endl;
 		cin >> numPlay;
 		if (numPlay < 2 || numPlay > 6) {
@@ -94,44 +102,51 @@ void startUp(int numberOfPlayers) {
 		cin >> numberOfCountries;
 		numberOfCountriesAssigned = numberOfCountries / numberOfPlayers;
 		cout << "Will assign the countries to players in turn order!" << "\n" << endl;
-		cout << "Each player will receive " << numberOfCountriesAssigned << " countries at random.\n" << endl;
+		for (int i = 0; i < numberOfCountries / numberOfPlayers;i++) {
+			for (int j = 0; j < n; j++) {
+				player[j].setNumberOfCountriesOwned(player[j].getNumberOfCountriesOwned() + 1);// could show the name of the country
+			}
+		}
+		
+		for (int i = 0; i < n; i++) {//display how many countries each player received
+			cout << player[i].getName() << " received " << player[i].getNumberOfCountriesOwned() << " countries" << endl;
+		}
 
-
+		cout << "\n"<< endl;
 		//giving out the number of initial army for each player depending on the amount of players
-
 		if (numberOfPlayers == 2) {
-			cout << "Each player receives 40 armies. \n" << endl;
+			
 			for (int i = 0; i < n; i++) {
 				player[i].setArmiesToPlace(40);
-				cout << player[i].getName() << " received " << player[i].getArmies() << endl;
+				cout << player[i].getName() << " received " << player[i].getArmiesToPlace() << endl;
 			}
 		}
 		else if (numberOfPlayers == 3) {
-			cout << "Each player receives 35 armies.\n" << endl;
+			
 			for (int i = 0; i < n; i++) {
 				player[i].setArmiesToPlace(35);
-				cout << player[i].getName() << " received " << player[i].getArmies() << endl;
+				cout << player[i].getName() << " received " << player[i].getArmiesToPlace() << endl;
 			}
 		}
 		else if (numberOfPlayers == 4) {
-			cout << "Each player receives 30 armies.\n" << endl;
+			
 			for (int i = 0; i < n; i++) {
 				player[i].setArmiesToPlace(30);
-				cout << player[i].getName() << " received " << player[i].getArmies() << endl;
+				cout << player[i].getName() << " received " << player[i].getArmiesToPlace() << endl;
 			}
 		}
 		else if (numberOfPlayers == 5) {
-			cout << "Each player receives 25 armies.\n" << endl;
+			
 			for (int i = 0; i < n; i++) {
 				player[i].setArmiesToPlace(25);
-				cout << player[i].getName() << " received " << player[i].getArmies() << endl;
+				cout << player[i].getName() << " received " << player[i].getArmiesToPlace() << endl;
 			}
 		}
 		else if (numberOfPlayers == 6) {
-			cout << "Each player receives 20 armies.\n" << endl;
+			
 			for (int i = 0; i < n; i++) {
 				player[i].setArmiesToPlace(20);
-				cout << player[i].getName() << " received " << player[i].getArmies() << endl;
+				cout << player[i].getName() << " received " << player[i].getArmiesToPlace() << endl;
 			}
 		}
 }
