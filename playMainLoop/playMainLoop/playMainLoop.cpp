f333da7cd8f9d72cd4d90c8e144e21b7a05623cf
Sebastien Ong Tone <s.ongtone@hotmail.com>
2017-10-26 23:51:09 -0400

    made a quick loop for part 3

--- playMainLoop/playMainLoop/playMainLoop.cpp
+++ playMainLoop/playMainLoop/playMainLoop.cpp
@@ -2,56 +2,93 @@
 //
 
 #include "stdafx.h"
+#include "playMainLoop.h"
 #include <iostream>
 using namespace std;
 
-void mainPlayerLoop();
+
 
 int main() {
-
-	mainPlayerLoop();
+	while (playerNumber) {//loop for the start up function
+		cout << "How many players?" << endl;
+		cin >> numPlay;
+		if (numPlay < 2 || numPlay > 6) {
+			cout << "Invalid number of players!" << endl;
+			cout << "Please try again!" << endl;
+		}
+		else {
+			mainPlayerLoop(numPlay);
+			playerNumber = false;
+		}
+	}
 	system("pause");
 	return 0;
 }
 
-void mainPlayerLoop() {
+void mainPlayerLoop(int numberOfPlayers) {
+	string playerName;
+	bool noWinner = true;
+	bool ownAllCountries = false;
 	bool phases = true;
 	bool rF = true;
 	int x;
+	int n;
+	players *player;
+	player = new players[numberOfPlayers];
+	n = numberOfPlayers;
 
-	while (phases) {
-		cout << "Choose an option" << endl;
-		cout << "1. Reinforce" << endl;
-		cout << "2. Attack" << endl;
-		cout << "3. Fortify \n" << endl;
-		cin >> x;
-		cout << endl;
-		
-		switch (x) {
-		case 1 : if (rF == true) {
-			cout << "Reinforcement Initiated!" << endl;
-			cout << "Reinforce function should be here. \n" << endl; // reinforcement() function here
-			rF = false;
+	while (turnOrder) {//determining the turn order. we can put the players in an array.
+		for (int i = 0; i < n; i++) {
+			cout << "Player " << i + 1 << ", enter your name!" << endl;
+			cin >> playerName;
+			player[i].setName(playerName);
+			turnOrder = false;
 		}
-				  else if (rF == false) {
-					  cout << "Reinforcement can only be used once." << endl;
-						  cout << "Choose a valid option \n" << endl;
-		}
-				  break;
-		case 2 : cout << "Attack Initiated!" << endl;
-			cout << "Attack function should be here. \n" << endl; //attack() function should be here
-			cout << " If this players owns all countries, the game will end" << endl;
-			break;
-		
-		case 3 : cout << "Fortification Initiated!" << endl;
-			cout << "Fortification Function should be here" << endl;
-			cout << "Next player function should be here" << endl;
-			phases = false;
-			break;
-		}
-
 	}
-}
+	while (noWinner) {
+		for (int i = 0; i < n; i++) {
+			cout << player[i].getName() << endl;
+			while (phases) {
+
+				cout << "Choose an option" << endl;
+				cout << "1. Reinforce" << endl;
+				cout << "2. Attack" << endl;
+				cout << "3. Fortify \n" << endl;
+				cin >> x;
+				cout << endl;
+
+				switch (x) {
+				case 1: if (rF == true) {
+					cout << "Reinforcement Initiated!" << endl;
+					cout << "Reinforce function should be here. \n" << endl; // reinforcement() function here
+					rF = false;
+				}
+						else if (rF == false) {
+							cout << "Reinforcement can only be used once." << endl;
+							cout << "Choose a valid option \n" << endl;
+						}
+						break;
+				case 2: cout << "Attack Initiated!" << endl;
+					cout << "Attack function should be here. \n" << endl; //attack() function should be here
+					ownAllCountries = true;
+					if (ownAllCountries) {
+						cout << " WE HAVE A WINNER " << endl;
+						noWinner = false;
+						phases = false;
+					}
+					break;
+
+				case 3: cout << "Fortification Initiated!" << endl;
+					cout << "Fortification Function should be here" << endl;
+					cout << "Next player function should be here" << endl;
+					phases = false;
+					break;
+				}
+
+			}
+		}
+	}
+	}
 
 
 
---
+++ playMainLoop/playMainLoop/playMainLoop.h
@@ -0,0 +1,109 @@
+#ifndef STARTUPPHASE_H
+#define STARTUPPHASE_H
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class players {//player class
+
+public:
+	int rolled;
+	int armyToPlace;
+	int armySize;
+	int numberOfCountriesOwned;
+
+	string name;
+
+	void setPlayerTurnOrderRoll(int roll) {//the players roll for the turn order
+		rolled = roll;
+	}
+	int getPlayerTurnOrderRoll() { return rolled; }
+
+	void setName(string s) {//set the names of the players
+		name = s;
+	}
+	string getName() { return name; }
+
+	void setArmySize(int ar) {
+		armySize = ar;
+	}
+	int getArmySize() { return armySize; }
+
+	void setArmiesToPlace(int a) {
+		armyToPlace = a;
+	}
+	int getArmiesToPlace() { return armyToPlace; }
+
+	void setNumberOfCountriesOwned(int c) {
+		numberOfCountriesOwned = c;
+	}
+	int getNumberOfCountriesOwned() { return numberOfCountriesOwned; }
+
+	players() : rolled(0), armyToPlace(0), name("Player 0"), numberOfCountriesOwned(0) {}//default constructor
+
+	players(const players& p)//copy constructor
+	{
+		name = p.name;
+		rolled = p.rolled;
+		armyToPlace = p.armyToPlace;
+		numberOfCountriesOwned = p.numberOfCountriesOwned;
+	}
+
+
+};
+
+class countries {//country class
+public:
+
+	string countryName;
+	string owner;
+	int armyOnCountry = 0;
+	int countryRando;
+
+	void setCountryRandoRoll(int rand) {
+		countryRando = rand;
+	}
+	int getCountryRandoRoll() { return countryRando; }
+	void setCountryName(string cName) {
+		countryName = cName;
+	}
+	string getCountryName() { return countryName; }
+	void setOwner(string id) {
+		owner = id;
+	}
+	string getOwner() { return owner; }
+	void setArmyOnCountry(int army) {
+		armyOnCountry = army;
+	}
+	int getArmyOnCountry() { return armyOnCountry; }
+
+	countries() : owner("default"), countryName("default"), armyOnCountry(0) {} // default constructor
+
+	countries(const countries& c) {
+		owner = c.owner;
+		countryName = c.countryName;
+		armyOnCountry = c.armyOnCountry;
+	}
+};
+
+int tieCounter = 0;
+int innerTieCount = 1;
+int numPlay;
+int numberOfCountriesAssigned;
+int numberOfCountries;
+
+bool playerNumber = true;
+bool turnOrder = true;
+bool showInfo;
+
+string answer;
+
+
+void mainPlayerLoop(int numberOfPlayers);
+
+
+
+
+#endif STARTUPPHASE_H
